20b593d10705ea9af24e5011002feb9334b247fa
Carlos Patino <carlospatinos@gmail.com>
2018-03-29 14:25:34 +0100

    Adding basic flows

---
+++ routes/api.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.status(200);
+  res.json({message: "accessing api"});
+});
+
+module.exports = router;
---
+++ test/app.js
@@ -0,0 +1,32 @@
+process.env.NODE_ENV = 'test';
+
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let should = chai.should();
+let app = require('../server');
+
+
+chai.use(chaiHttp);
+
+//Our parent block
+describe('APP', () => {
+    beforeEach((done) => { //Before each test we empty the database
+        done();   
+    });
+
+    /*
+    * Test the /GET route
+    */
+    describe('Basic route', () => {
+        it('it should GET response', (done) => {
+            chai.request(app)
+                .get('/app')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                done();
+                });
+        });
+    });
+});
